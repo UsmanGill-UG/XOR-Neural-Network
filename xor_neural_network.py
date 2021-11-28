2701028c340b20dc9cfa7586a6a9f94075e9eb46
Usman Gill <osmangill10@gmail.com>
2021-11-28 14:28:38 +0500

    updated feed forward
    using matrix representation now

--- xor_neural_network.py
+++ xor_neural_network.py
@@ -1,40 +1,54 @@
 import numpy as np
 
 
-class perceptron:
-    def __init__(self, x, y, xw, yw, b):  # constructor
-        self.x = x
-        self.y = y
-        self.xw = xw
-        self.yw = yw
-        self.b = b
-
-
+# activation function, we are using is sigmoid, returns 1 or 0
 def sigmoid(x):
-    res = 1 / (1 + np.exp(-x))
-    if res > 0.5:
-        return 1
-    else:
-        return 0
+    return 1 / (1 + np.exp(-x))
 
 
-def get_y_val(y):
-    #print((y.x * y.xw) + (y.y * y.yw) + y.b)
-    return sigmoid((y.x * y.xw) + (y.y * y.yw) + y.b)
+def Initialize_Parameters(input_f, num_of_neurons, output_f):
+    w1 = np.random.randn(num_of_neurons, input_f)  # 2 x2
+    w2 = np.random.randn(output_f, num_of_neurons)  # 2 x 1
+    b1 = np.zeros((num_of_neurons, 1))  # 2 x 1
+    b2 = np.zeros((output_f, 1))  # 1 x 1
+    print("w1 : ", w1)
+    print("w2 : ", w2)
+    print("b1 : ", b1)
+    print("b2 : ", b2)
+    return w1, w2, b1, b2
+
+
+def Forward_Propagation(X, Y, w1, w2, b1, b2):
+    z1 = np.dot(w1, X) + b1  # hidden layer
+    z1_hat = sigmoid(z1)
+    z2 = np.dot(w2, z1_hat) + b2  # z2 is final output/layer
+    z2_hat = sigmoid(z2)
+    mse = np.mean(np.square(Y - z2_hat))  # mean square error
+    return mse, z1, z1_hat, z2, z2_hat
+
+
+def Back_Propagation():
+    lol = 0
 
 
 def main():
-    x = 0
-    y = 0
-    y1 = perceptron(x, y, 1, 1, -1)
-    y1_val = get_y_val(y1)
-    y2 = perceptron(x, y, -1, -1, 1)
-    y2_val = get_y_val(y2)
-    y3 = perceptron(y1_val, y2_val, 1, 1, -2)
-    y3_val = get_y_val(y3)
-    print("y1 : ", y1_val)
-    print("y2 : ", y2_val)
-    print("y3 : ", y3_val)
+    input_xor = np.array([[0, 0, 1, 1], [0, 1, 0, 1]])  # input
+    output_xor = np.array([[0, 1, 1, 0]])  # output
+    # print(input_xor)
+    # input_xor.shape()  # giving error : 'tuple' object is not callable
+    num_of_neurons = 2
+    input_f = input_xor.shape[0]
+    output_f = output_xor.shape[0]
+    w1, w2, b1, b2 = Initialize_Parameters(input_f, num_of_neurons, output_f)
+    epoch = 5000
+    lr = 0.01
+    total_loss = np.zeros((epoch, 1))
+    mse, z1, z1_hat, z2, z2_hat = Forward_Propagation(input_xor, output_f, w1, w2, b1, b2)
+    print("z1 : ", z1)
+    print("z1 hat : ", z1_hat)
+    print("z2 : ", z2)
+    print("z2_hat : ", z2_hat)
+    print("mse : ", mse)
 
 
 if __name__ == '__main__':
